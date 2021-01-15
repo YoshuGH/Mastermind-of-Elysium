4e037efc654255f454afbc424525e1bae01359de
YoshuGH <axeleonardo2000@gmail.com>
2021-01-15 00:52:57 -0600

    AI v1.0

--- IA for VR/Assets/IA for VR/Scripts/Players/PlanetAgent.cs
+++ IA for VR/Assets/IA for VR/Scripts/Players/PlanetAgent.cs
@@ -11,68 +11,209 @@ public class PlanetAgent : Agent
     `      Caminos
             Estado del objetivo
      * */
-    [SerializeField]private BattleGround battleGround;
+    [SerializeField] private BattleGround battleGround;
+    [SerializeField] private GameManager gameManager;
     private Player player;
-    public bool isNodeFree;
-    public List<Node> nodesInControl;
-    private bool state;
-    public List<Node> nodeWaypoints;
-    public Node targetNode, currentNode;
+    private int oldPlayerNodeCount;
+    //public bool isNodeFree;
+    //public List<Node> nodesInControl;
+    //public List<Node> nodeWaypoints;
 
-    private void Start() {
-        player = GetComponent<Player>();   
+    private void Awake()
+    {
+        player = GetComponent<Player>();
     }
+    /*
+public override void Initialize()
+{
+    player = GetComponent<Player>();
+    battleGround = GetComponentInParent<BattleGround>();
+    battleGround.Nodes = new List<Node>();
+    //battleGround.NodeQty = battleGround.GenerateRandomNodeQty(2);
+    oldPlayerNodeCount = player.PlayerNodes.Count;
+    gameManager.ResetEverything();
+}*/
 
     public override void OnEpisodeBegin()
     {
-        nodesInControl = player.PlayerNodes;
-        currentNode = nodesInControl[0];
-        nodeWaypoints = currentNode.Neighbors;
+        gameManager.ResetEverything();
+    }
+
+    private void FixedUpdate()
+    {
+        //print(gameManager.MapExists);
+        if (gameManager.MapExists)
+        {
+            RequestDecision();
+        }
     }
 
     public override void CollectObservations(VectorSensor sensor)
     {
-        sensor.AddObservation(currentNode);
-        sensor.AddObservation(nodeWaypoints.Count);
-        sensor.AddObservation(isNodeFree);
-        sensor.AddObservation(targetNode);
-        sensor.AddObservation(nodesInControl.Count);
+        //Si esta en idle
+        if(player.Idle)
+        {
+            // Que sepa la cantidad de nodos que tiene en su control
+            sensor.AddObservation(player.PlayerNodes.Count);
+            // Que sepa la posicion del nodo de su lista de nodos en el que esta
+            sensor.AddObservation(player.Idle_SelectedNodeIndex);
+            // Que sepa en que estado se encuentra
+            sensor.AddObservation(player.State);
+        }
+        else if(player.SelectingNode)
+        {
+            // Que sepa la cantidad de nodos que se puede mover
+            sensor.AddObservation(player.Idle_SelectedNode.Neighbors.Count);
+            // Que sepa la posicion del nodo de si lista de nodos a los que se puede mover
+            sensor.AddObservation(player.Selecting_SelectedNodeIndex);
+            // Que sepa en que estado se encuentra
+            sensor.AddObservation(player.State);
+        }
     }
 
     public override void OnActionReceived(float[] vectorAction)
     {
-        Node prevCurrentNode = currentNode;
-        Node prevTargetNode = targetNode;
-        int prevNodesQty = nodesInControl.Count;
+        // Si no esta haciendo nada, es decir, si no esta seleccionando
+        #region Idle State
+        if (player.Idle)
+        {
+            // Al presionar la flecha itera hacia la derecha (es decir suma 1 al iterador),
+            // sobre la lista de nodos que el player posee
+            if (Mathf.FloorToInt(vectorAction[0]) == 1)
+            {
+                player.MoveRight(player.Idle_SelectedNodeIndex, player.PlayerNodes, 0);
+                print("Move right Idle" + player.Idle_SelectedNodeIndex);
+            }
+            // Al presionar la flecha itera hacia la izquierda (es decir resta 1 al iterador),
+            // sobre la lista de nodos que el player posee
+            if (Mathf.FloorToInt(vectorAction[0]) == 2)
+            {
+                player.MoveLeft(player.Idle_SelectedNodeIndex, player.PlayerNodes, 0);
+                print("Move left Idle" + player.Idle_SelectedNodeIndex);
+            }
 
-        currentNode = nodesInControl[(int)vectorAction[0]];
-        targetNode = nodeWaypoints[(int)vectorAction[1]];
-
-        //Cambio de nodo?
-        if(isTheSameNode(currentNode, prevCurrentNode)){
-
+            // Al presionar espacio entra en modo de seleccion de nodos
+            if (Mathf.FloorToInt(vectorAction[0]) == 3)
+            {
+                player.EnterSelectMode();
+            }
         }
-        //Cambio de destino?
-        if(isTheSameNode(targetNode, prevTargetNode))
+        #endregion
 
-        /// ----- Rewards ---- ///
+        // Si esta seleccionando un nodo
+        #region Selecting Node State
+        else if (player.SelectingNode)
+        {
+            // Al presionar la flecha itera hacia la derecha (es decir suma 1 al iterador),
+            // sobre la lista de nodos que haya detectado a su alrededor
+            if (Mathf.FloorToInt(vectorAction[0]) == 1)
+            {
 
-        //Si pierde un nodo(futuro)
+                player.MoveRight(player.Selecting_SelectedNodeIndex, player.Idle_SelectedNode.Neighbors, 1);
+                print("Move right Selecting" + player.Selecting_SelectedNodeIndex);
+            }
 
-        //Si no hace nada hay tabla
-        //Verifica si el nodo en el que esta y el nodo objetivo es exactamente el mismo que el step pasado
-        if(isTheSameNode(prevCurrentNode, currentNode) &&  isTheSameNode(prevTargetNode, targetNode)){
-            AddReward(-0.001f);
+            // Al presionar la flecha itera hacia la izquierda (es decir resta 1 al iterador),
+            // sobre la lista de nodos que haya detectado a su alrededor
+            if (Mathf.FloorToInt(vectorAction[0]) == 2)
+            {
+                player.MoveLeft(player.Selecting_SelectedNodeIndex, player.Idle_SelectedNode.Neighbors, 1);
+                print("Move left Selecting" + player.Selecting_SelectedNodeIndex);
+            }
+
+            if (Mathf.FloorToInt(vectorAction[0]) == 4)
+            {
+                player.SelectNode();
+            }
+
+            // Si vuelve a presionar espacio estando en seleccion, este cancela la seleccion
+            if (Mathf.FloorToInt(vectorAction[0]) == 3)
+            {
+                player.ExitSelectMode();
+            }
+        }
+        #endregion
+
+        if(oldPlayerNodeCount < player.PlayerNodes.Count)
+        {
+            AddReward(0.1f);
+            oldPlayerNodeCount = player.PlayerNodes.Count;
+        }
+        else if(oldPlayerNodeCount > player.PlayerNodes.Count)
+        {
+            AddReward(-0.3f);
+            oldPlayerNodeCount = player.PlayerNodes.Count;
         }
 
-        //Conquisto algo mi pana
-        if(nodesInControl.Count > prevNodesQty){
-            AddReward((nodesInControl.Count - prevNodesQty) * 0.01f);
+        if(battleGround.NodeQty == player.PlayerNodes.Count)
+        {
+            AddReward(1f);
+            EndEpisode();
         }
 
-        
+    }
 
+    public override void Heuristic(float[] actionsOut)
+    {
+        actionsOut[0] = 0;
 
+        // Si no esta haciendo nada, es decir, si no esta seleccionando
+        #region Idle State
+        if (player.Idle)
+        {
+            // Al presionar la flecha itera hacia la derecha (es decir suma 1 al iterador),
+            // sobre la lista de nodos que el player posee
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                actionsOut[0] = 1;
+            }
+            // Al presionar la flecha itera hacia la izquierda (es decir resta 1 al iterador),
+            // sobre la lista de nodos que el player posee
+            if (Input.GetKey("left"))
+            {
+                actionsOut[0] = 2;
+            }
+
+            // Al presionar espacio entra en modo de seleccion de nodos
+            if (Input.GetKey(KeyCode.Space))
+            {
+                actionsOut[0] = 3;
+
+            }
+        }
+        #endregion
+
+        // Si esta seleccionando un nodo
+        #region Selecting Node State
+        else if (player.SelectingNode)
+        {
+            // Al presionar la flecha itera hacia la derecha (es decir suma 1 al iterador),
+            // sobre la lista de nodos que haya detectado a su alrededor
+            if (Input.GetKey("right"))
+            {
+
+                actionsOut[0] = 1;
+            }
+
+            // Al presionar la flecha itera hacia la izquierda (es decir resta 1 al iterador),
+            // sobre la lista de nodos que haya detectado a su alrededor
+            if (Input.GetKey("left"))
+            {
+                actionsOut[0] = 2;
+            }
+
+            if (Input.GetKey("enter"))
+            {
+                actionsOut[0] = 4;
+            }
+
+            // Si vuelve a presionar espacio estando en seleccion, este cancela la seleccion
+            if (Input.GetKey(KeyCode.Space))
+            {
+                actionsOut[0] = 3;
+            }
+        }
+        #endregion
     }
 
     private bool isTheSameNode(Node node1, Node node2){
--- IA for VR/Assets/IA for VR/Scripts/Players/Player.cs
+++ IA for VR/Assets/IA for VR/Scripts/Players/Player.cs
@@ -15,6 +15,8 @@ public class Player : MonoBehaviour
     public int MaxshipsQty { get { return maxShipsQty; } }
     private int resources;
     public int Resources { get { return resources; } }
+    private int state = 0;
+    public int State { get { return state; } }
 
     public void AddResources(int _resourcesQty) => resources += _resourcesQty;
 
@@ -47,14 +49,16 @@ public class Player : MonoBehaviour
     //Accesores del Input Manager
     public Node Idle_SelectedNode { set { idleSelectedNode = value; } get { return idleSelectedNode; } }
     public Node Selecting_SelectedNode { set { selectingNodeSelectedNode = value; } get { return selectingNodeSelectedNode; } }
-    public int Idle_SelectedNodeIndex { set { selectingNodeSelectedNodeIndex = value; } get { return selectingNodeSelectedNodeIndex; } }
+    public int Idle_SelectedNodeIndex { set { idleSelectedNodeIndex = value; } get { return idleSelectedNodeIndex; } }
     public int Selecting_SelectedNodeIndex { set { selectingNodeSelectedNodeIndex = value; } get { return selectingNodeSelectedNodeIndex; } }
+    public bool Idle { get { return idle; } }
+    public bool SelectingNode { get { return selectingNode; } }
 
     // Start is called before the first frame update
     void Start()
     {
         // Activar el outline de seleccion del nodo, como al principio solo hay un nodo, se activa ese nada mas
-        Invoke("OutlineAtStart", 0.15f);
+        //Invoke("OutlineAtStart", 0.15f);
     }
 
     #region Input Manager
@@ -86,7 +90,7 @@ public class Player : MonoBehaviour
     }
 
     // Funcion que activa el Outline.cs del el primer y unico nodo al momento de iniciar el juego
-    void OutlineAtStart()
+    public void OutlineAtStart()
     {
         idleSelectedNode = playerNodes[idleSelectedNodeIndex];
         idleSelectedNode.GetComponentInParent<Outline>().enabled = true;
@@ -94,6 +98,7 @@ public class Player : MonoBehaviour
 
     public void MoveRight(int _iterator, List<Node> _listToIterate, int _state)
     {
+        //print(_iterator);
         if (_listToIterate.Count >= 1 && (_iterator + 1) < _listToIterate.Count)
         {
             ChangeSelectedNode(_listToIterate[_iterator + 1], _state);
@@ -106,6 +111,7 @@ public class Player : MonoBehaviour
 
     public void MoveLeft(int _iterator, List<Node> _listToIterate, int _state)
     {
+        //print(_iterator);
         if (_listToIterate.Count >= 1 && (_iterator - 1) >= 0)
         {
             ChangeSelectedNode(_listToIterate[_iterator - 1], _state);
@@ -126,6 +132,7 @@ public class Player : MonoBehaviour
     {
         selectingNode = true;
         idle = false;
+        state = 1;
         //selecField = Instantiate(selectionField, idleSelectedNode.GetComponentInParent<Transform>().localPosition, idleSelectedNode.GetComponentInParent<Transform>().localRotation);
         //selecField.transform.localScale = new Vector3(selectionFieldScale, selectionFieldScale, selectionFieldScale);
 
@@ -139,11 +146,13 @@ public class Player : MonoBehaviour
     {
         idle = true;
         selectingNode = false;
+        state = 0;
         selectingNodeSelectedNode.GetComponentInParent<Outline>().enabled = false;
         idleSelectedNode.GetComponentInParent<Outline>().OutlineColor = Color.yellow;
         //Destroy(selecField);
         //selecField = null;
         selectingNodeSelectedNode = null;
+        selectingNodeSelectedNodeIndex = 0;
     }
 
     public void SelectNode()
@@ -158,6 +167,7 @@ public class Player : MonoBehaviour
         // Cancelar la seleccion
         idle = true;
         selectingNode = false;
+        state = 0;
         selectingNodeSelectedNode.GetComponentInParent<Outline>().enabled = false;
         idleSelectedNode.GetComponentInParent<Outline>().OutlineColor = Color.yellow;
         //Destroy(selecField);
